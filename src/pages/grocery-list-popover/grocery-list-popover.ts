a514e29cf83f12f4bf1581e45fcca2d8a6426570
jacobhallberg <jaha7212@colorado.edu>
2017-04-16 18:36:36 -0600

    Added Rename funcitonality. Using Delete and Create functions located inside of the grocery_list_popover page.

--- src/pages/grocery-list-popover/grocery-list-popover.ts
+++ src/pages/grocery-list-popover/grocery-list-popover.ts
@@ -66,11 +66,19 @@ export class GroceryListPopoverPage {
           console.log(this.change);
           console.log(this.object);
           console.log(this.groceryList.id);
-          this.backandService.delete('grocery_list', this.groceryList.id);
+          this.delete();
           this.groceryList.name = this.change;
           console.log(this.groceryList.name);
           console.log(this.groceryList);
-          this.backandService.create('grocery_list', this.groceryList);
+          // grocery_list is a table we created in model.json
+          this.backandService.create('grocery_list', this.groceryList)
+            .subscribe(
+              data => {
+                console.log('Returned from create', data);
+              },
+              err => this.backandService.logError(err),
+              () => console.log('OK')
+            );
         }
       }
     ]
@@ -87,7 +95,25 @@ export class GroceryListPopoverPage {
     console.log('you should implement share');
   }
 
+ // public delete(object: string, id: string) {
   delete() {
-    console.log('you should implement delete');
+    console.log('deleting', this.groceryList);
+    // grocery_list is a table we created in model.json
+    this.backandService.delete('grocery_list', this.groceryList.id)
+    // subscribe to response from the server so we are notified when the asynchronous result is available
+      .subscribe(
+          // 1st argument is function to run when the operation is a success
+          data => {
+              console.log('returned from delete', data);
+              //close the popover
+              this.close();
+          },
+          // 2nd argument is function to run when the operation fails
+          err => this.backandService.logError(err),
+          // 3rd operation is function to run when the observable is done (in
+          // this case the asynchronous operation is the observable)
+          () => console.log('OK')
+      );
   }
 }
+
